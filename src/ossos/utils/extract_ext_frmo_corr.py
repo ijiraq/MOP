7cbb3da7cf227840c172a3369881209f61c0a35d
JJ Kavelaars <JJ.Kavelaars@nrc-cnrc.gc.ca>
2024-07-05 07:04:53 +0800

    Extract image from LSST MEF

---
+++ src/ossos/utils/extract_ext_frmo_corr.py
@@ -0,0 +1,31 @@
+"""
+Given an LSST Pipepeline CORR file extact the PRIMARY header and IMAGE from extension 1 and store as SIF
+"""
+from astropy.io import fits
+import argparse
+import re
+
+
+def extract_image(filename):
+    with fits.open(filename) as hdulist:
+        ccdno = int(hdulist[0].header['T_SDOID'])
+        expid = hdulist[0].header['EXP-ID']
+        beeid = int(hdulist[0].header['T_BEEID'])
+        expnum = int(re.match('HSCE(\d+)', expid).group(1)) + beeid
+
+        out_filename = f"{expnum}p{ccdno}.fits"
+        new_hdu = hdulist[0]
+        new_hdu.data = hdulist[1].data
+        for keyword in hdulist[1].header:
+            if keyword == 'EXTEND':
+                continue
+            new_hdu.header[keyword] = hdulist[1].header[keyword]
+        new_hdu.writeto(out_filename)
+    
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help="Name of CORR file to extract IMAGE extension from")
+    args = parser.parse_args()
+    extract_image(args.filename)
+
