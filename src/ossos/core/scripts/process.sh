187f347836f43a5de2d1cea0af71b8d9b29c9b73
JJ Kavelaars <JJ.Kavelaars@nrc-cnrc.gc.ca>
2024-07-09 23:08:49 -0700

    Correct CCD and DETECTOR numbering to be 0 padded and got to 100.

--- src/ossos/core/scripts/process.sh
+++ src/ossos/core/scripts/process.sh
@@ -108,7 +108,8 @@ do
     --ang "${angle}" --width ${plant_width} --num ${num}
   preix="fk"
   do_search
-  astrom_mag_check_novos "${expnums[0]}" "${ccdnum}" --expnum "${expnums[0]}" --astrom-filename "fk${expnums[0]}p${ccdnum}.measure3.cands.astrom" --fk --type p
+  ccdstr="echo ${ccdnum}|awk '{printf(\"%02d\",$1)}'"
+  astrom_mag_check_novos "${expnums[0]}" "${ccdnum}" --expnum "${expnums[0]}" --astrom-filename "fk${expnums[0]}p${ccdstr}.measure3.cands.astrom" --fk --type p
 done
 
 
